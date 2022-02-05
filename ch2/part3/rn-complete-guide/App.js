e13b4fee6e32e0cdbd8bcb3789587fa3cb0b5efc
nancologist <nancologist@gmail.com>
2022-02-05 12:25:01 +0100

    Refactor

--- ch2/part3/rn-complete-guide/App.js
+++ ch2/part3/rn-complete-guide/App.js
@@ -4,13 +4,13 @@ export default function App() {
   return (
     <View style={styles.screen}>
       <View
-          style={styles.inputContainer}
+        style={styles.inputContainer}
       >
         <TextInput
-            placeholder="Your Goals..."
-            style={styles.input}
+          placeholder="Your Goals..."
+          style={styles.input}
         />
-        <Button title="Add" />
+        <Button title="Add"/>
       </View>
       <View>
         {/* List of Goals Placeholder */}
@@ -20,18 +20,18 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        padding: 50
-    },
-    inputContainer: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        justifyContent: 'space-between'
-    },
-    input: {
-        borderColor: 'black',
-        borderWidth: 1,
-        padding: 10,
-        width: '80%'
-    }
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  input: {
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+    width: '80%'
+  }
 });
