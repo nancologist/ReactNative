b6a30bf4dcce2e85d6b32e42eddf6aa64ec3d234
nancologist <nancologist@gmail.com>
2022-02-08 11:19:27 +0100

    ## 4.4. Adding a Screen Component

---
+++ ch4/app/components/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Header = props => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.headerTitle}>{props.title}</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: 'center',
+    backgroundColor: '#f7287b',
+    height: 90,
+    justifyContent: 'center',
+    paddingTop: 36,
+    width: '100%',
+  },
+  headerTitle: {
+    color: 'black'
+  }
+});
+
+export default Header;
