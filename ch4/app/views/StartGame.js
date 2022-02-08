299652067aa0232f07734afbc0db9bf6658e8479
nancologist <nancologist@gmail.com>
2022-02-08 11:32:48 +0100

    ## 4.5. Working on the Layout

--- ch4/app/views/StartGame.js
+++ ch4/app/views/StartGame.js
@@ -1,9 +1,20 @@
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, TextInput, Button} from 'react-native';
 
 const StartGame = props => {
   return (
     <View style={styles.screen}>
-      <Text>Start-Game Screen</Text>
+      <Text>Start a New Game!</Text>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.title}>Select a Number</Text>
+        <TextInput />
+
+        <View style={styles.buttonContainer}>
+          <Button title={'Reset'} onPress={() => {}} />
+          <Button title={'Confirm'} onPress={() => {}} />
+        </View>
+
+      </View>
     </View>
   );
 };
@@ -13,6 +24,21 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15
   }
 });
 
