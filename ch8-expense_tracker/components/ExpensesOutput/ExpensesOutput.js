2b7c4cb4ca972ce397fbd4664cf681366b30601c
Morteza Jalilifar <morteza.jalilifar@netgo.de>
2024-12-25 14:25:14 +0100

    ## 8.19. Managing App-wide State With Context

--- ch8-expense_tracker/components/ExpensesOutput/ExpensesOutput.js
+++ ch8-expense_tracker/components/ExpensesOutput/ExpensesOutput.js
@@ -3,57 +3,6 @@ import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from '../ExpensesList/ExpensesList';
 import ExpensesSummary from '../ExpensesSummary/ExpensesSummary';
 
-const dummyExpenses = [
-  {
-    id: 'e1',
-    desc: 'A pair of shoes',
-    amount: 59.99,
-    date: new Date('2021-12-19')
-  },
-  {
-    id: 'e2',
-    desc: 'A pair of trousers',
-    amount: 89.29,
-    date: new Date('2022-01-05')
-  },
-  {
-    id: 'e3',
-    desc: 'some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e4',
-    desc: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19')
-  },
-  {
-    id: 'e5',
-    desc: 'A book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  },
-  {
-    id: 'e6',
-    desc: 'some bananas',
-    amount: 5.99,
-    date: new Date('2021-12-01')
-  },
-  {
-    id: 'e7',
-    desc: 'A book',
-    amount: 14.99,
-    date: new Date('2022-02-19')
-  },
-  {
-    id: 'e8',
-    desc: 'A book',
-    amount: 18.59,
-    date: new Date('2022-02-18')
-  }
-]
-
 function ExpensesOutput({ expenses, period }) {
   return (
     <View style={styles.container}>
---
+++ ch8-expense_tracker/expense-model.js
@@ -0,0 +1,7 @@
+export class ExpenseDto {
+    constructor(description, amount, date) {
+        this.description = description;
+        this.amount = amount;
+        this.date = date;
+    }
+}
